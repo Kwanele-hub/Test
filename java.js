34089bfc46e34123285059d51a9fe7e51ba5922e
nolwazi749 <nolwazizwane7@gmail.com>
2020-07-16 06:28:55 -0400

    add

---
+++ java.js
@@ -0,0 +1,13 @@
+<html> 
+    <head> 
+        <script type="text/javascript" src= 
+        "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.9.1/underscore.js"> 
+        </script> 
+    </head> 
+    <body> 
+        <script type="text/javascript"> 
+            var numbers = [100, 50, 400, 66, 7900]; 
+            console.log(_.min(numbers)); 
+        </script> 
+    </body> 
+</html>                     
